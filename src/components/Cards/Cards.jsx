65692a5d2ed115d59ee4ec98b82d286c07311f25
Franciele Zanella <36767316+Franzanella@users.noreply.github.com>
2020-07-29 16:58:10 -0300

    projeto no git

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import CardComponent from './Card/Card';
+
+import styles from './Cards.module.css';
+
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Loading...';
+  }
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <CardComponent
+          className={styles.infected}
+          cardTitle="Infectados"
+          value={confirmed.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Número de casos ativos do COVID-19."
+        />
+        <CardComponent
+          className={styles.recovered}
+          cardTitle="Recuperados"
+          value={recovered.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Número de recuperações de COVID-19."
+        />
+        <CardComponent
+          className={styles.deaths}
+          cardTitle="Mortes"
+          value={deaths.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Número de mortes causadas por COVID-19."
+        />
+      </Grid>
+    </div>
+  );
+};
+export default Info;
